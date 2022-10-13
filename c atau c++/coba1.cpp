3b37e8d977e283c1dd0f09af151d1af874f2492b
alvoera2 <alimrisnanto@gmail.com>
2022-10-13 11:26:09 +0700

    Menambahkan File Latihan C/C++ dan PHP

---
+++ c atau c++/coba1.cpp
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <conio.h>
+main(){
+    int a = 11;
+    int b = 2;
+    char c[13] = "Alim Purnomo";
+
+    puts("Nama ");
+    puts(c);
+    printf("Terdiri dari %d Hurup dan %d kata", a, b);
+}
